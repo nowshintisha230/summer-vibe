224f458905fde90bebd904ccdf750294b9ab2728
Rieadh Sayfullah <you@example.com>
2026-05-02 11:56:49 +0600

    data shown in homepage

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,14 @@ const nextConfig = {
 
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
+      
       {
         protocol: "https",
         hostname: "i.pinimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "tse1.mm.bing.net",
       },
     ],
   },
